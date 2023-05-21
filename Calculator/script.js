95dfe923491cefd878a019bda8c6075a6aa526b9
charan kodali <charankodali3@gmail.com>
2023-05-21 21:08:42 +0530

    improved-javascript

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculateResult() {
+    const result = document.getElementById('result').value;
+    const calculation = eval(result);
+    document.getElementById('result').value = calculation;
+  }
+  
